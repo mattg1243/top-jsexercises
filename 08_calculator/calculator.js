6f6760304e9f1964695b2cb2a9dde57e49ba7829
mattg1243 <mattgallucci97@gmail.com>
2021-08-18 21:18:41 -0700

    finished all calculator functions, lots of good stuff in here that ill be using later

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
+const sum = function(a) {
 	
+  if (a.length !== 0) {
+    return a.reduce((accum, currentValue) => accum + currentValue, 0)
+  } else {
+    return 0;
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  
+  if (a.length !== 0) {
+    return a.reduce((accum, currentValue) => accum * currentValue)
+  } else {
+    return 0;
+  }
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a < 0) {
+    return -1;
+  } else if (a === 0) {
+    return 1;
+  } else {
+  return (a * factorial(a-1))}
 };
 
 module.exports = {
