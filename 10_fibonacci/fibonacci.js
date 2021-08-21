ed8d4bb2be4b8c958f546ff8508db7428ec800d8
mattg1243 <mattgallucci97@gmail.com>
2021-08-21 00:56:30 -0700

    fib done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(arg) {
+    
+    let fib = [];
 
+    if (arg < 0) {
+        return 'OOPS';
+    }
+
+    if (typeof arg === 'string') {
+        intArg = parseInt(arg, 10);
+    }
+
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (let i = 2; i <= arg; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+        console.log(fib[i]);
+    }
+    
+    return fib[arg];
 };
 
 module.exports = fibonacci;
