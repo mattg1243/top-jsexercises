82d86c4f9324dc064f64b07006dd348a2292477a
mattg1243 <mattgallucci97@gmail.com>
2021-08-20 21:22:39 -0700

    had the first test passing at first but now not working, about to try to a decrementing for loop instead of popping out of a dubplicate array

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,32 @@
-const palindromes = function () {
+const palindromes = function (str) {
+   
+    let strL = str.toLowerCase();
+    let reversed = [];
+    let strArr = Array.from(strL.split(''));
+    let strArrCopy = strArr;
+    let argLength = strArr.length;
 
+    console.log(strArrCopy);
+
+    for (let i = 0; i < argLength; i++) {
+        let lastChar = strArrCopy.pop();
+        if (lastChar.match(/^[0-9a-z]+$/)) {
+            reversed.push(lastChar);
+        }
+    }
+
+    console.log(reversed);
+    console.log(strArr);
+    
+    for(let i = 0; i < argLength - 1; i++) {
+        if (strArr[i] !== reversed[i]) {
+            console.log('strArr[i] == ' + strArr[i] + '    reversed[i] == ' + reversed[i]);
+            return false;
+        }
+    }
+    
+    return true;
 };
 
+console.log(palindromes('racecar'));
 module.exports = palindromes;
